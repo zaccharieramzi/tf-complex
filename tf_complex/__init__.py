46c56bb313bc6faa865048660b055468469b71ff
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-28 12:07:58 +0200

    added references to init

---
+++ tf_complex/__init__.py
@@ -0,0 +1,9 @@
+"""Complex neural networks utilities
+
+References
+----------
+
+..[C2020] Elizabeth Cole et al. (2020)
+    Complex-Valued Convolutional Neural Networks for MRI Reconstruction
+    https://arxiv.org/abs/2004.01738
+"""
