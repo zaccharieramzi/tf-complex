cbcd5d4e3f324d3d89edca40b2d884d69893f7e9
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-28 12:03:13 +0200

    WIP started working on activation functions

---
+++ tf_complex/activations.py
@@ -0,0 +1,14 @@
+from tensorflow.keras.layers import Layer
+
+class ZReLU(Layer):
+    def call(self, inputs):
+        pass
+
+
+class ModReLU(Layer):
+    def call(self, inputs):
+        pass
+
+class Cardioid(Layer):
+    def call(self, inputs):
+        pass
