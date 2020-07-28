a2e13b93b9bfd1533273d815ba3248c11b8400ef
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-07-28 13:14:52 +0200

    added setup for tf complex

--- tf_complex/__init__.py
+++ tf_complex/__init__.py
@@ -7,3 +7,10 @@ References
     Complex-Valued Convolutional Neural Networks for MRI Reconstruction
     https://arxiv.org/abs/2004.01738
 """
+
+__version__ = '0.0.1'
+__author__ = 'Zaccharie Ramzi'
+__author_email__ = 'zaccharie.ramzi@inria.fr'
+__license__ = 'MIT'
+__homepage__ = 'https://github.com/zaccharieramzi/tf-complex'
+__docs__ = 'TensorFlow utilities for complex neural networks.'
